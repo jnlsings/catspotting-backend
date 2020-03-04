6573f1539c9c6e4750ada43a103ba26725e279d9
jnlsings <jnlsings@gmail.com>
2020-03-04 11:02:24 -0600

    Set up app, .gitignore, .env

---
+++ catspotting_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CatspottingBackendConfig(AppConfig):
+    name = 'catspotting_backend'
---
+++ settings.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE catspotting_backend;
+
+CREATE USER catspotting_backend_user WITH PASSWORD 'catspotting_backend';
+
+GRANT ALL PRIVILEGES ON DATABASE catspotting_backend TO catspotting_backend_user;
