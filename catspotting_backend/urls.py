59fca3c4223b4cb4c1745c999370f4bdfb98b849
javascriptures <jtjavascripture@gmail.com>
2020-03-04 12:35:43 -0800

    urls + views

---
+++ catspotting_backend/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('comments/', views.CommentList.as_view(), name='comment_list'),
+    path('comments/<int:pk>', views.CommentDetail.as_view(), name='comment_detail'),
+]
--- catspotting_backend/views.py
+++ catspotting_backend/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
 
 # Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
