b56d09f816ce29102e2a41bcfe184275b5441607
jnlsings <jnlsings@gmail.com>
2020-03-04 13:53:45 -0600

    Set up models, migrate

--- catspotting_backend/models.py
+++ catspotting_backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.CharField(max_length=1000)
+    body = models.CharField(max_length=1000)
+    img_url = models.TextField()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=1000)
+    body = models.CharField(max_length=1000)
