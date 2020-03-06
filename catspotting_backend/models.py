a7a12a3b606eed80ed622c52d86b0f36f6d52106
jnlsings <jnlsings@gmail.com>
2020-03-05 21:43:30 -0600

    Remove commented out user model

--- catspotting_backend/models.py
+++ catspotting_backend/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
-
-# class User(models.Model):
-#     name =
-#     user_name = models.CharField(max_length=15)
+# Create your models here
 
 
 class Post(models.Model):
